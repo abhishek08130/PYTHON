03d68dd02c556b59b0f79f9706f9cd4a64a3c072
abhishek08130 <abhishekbhatt08130@gmail.com>
2024-11-11 12:40:22 +0530

    Create Comments escaplesequence &print in python.py

---
+++ Comments escaplesequence &print in python.py
@@ -0,0 +1,10 @@
+'''
+this is comment
+this is comment
+this is comment
+'''
+# this is comment
+# this is comment
+# this is comment
+print("Hello\nWorld")# \n creates a new line
+print("hey",6,7, sep="~",end="009\n")# \t creates a tab
