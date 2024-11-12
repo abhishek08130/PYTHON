577c1068beb3eb2d7218c2a1c61489edcce5e9be
abhishek08130 <abhishekbhatt08130@gmail.com>
2024-11-12 23:44:13 +0530

    duble line string

--- input function.py
+++ input function.py
@@ -1,2 +1,11 @@
-a=input("Enter a text: ")
-print(a)
+# a=input("Enter a text: ")
+# print(a)
+
+x=int(input("Enter a number: "))
+y=int(input("Enter another number: "))
+print(x+y)
+
+a='''hello abhi
+ hi abhi 
+ by abhi'''
+ print(a)
