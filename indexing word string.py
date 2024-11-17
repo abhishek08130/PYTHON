f182fbe1bc529c314c10c970bd14ff6b89ae47e3
abhishek08130 <abhishekbhatt08130@gmail.com>
2024-11-17 22:25:01 +0530

    added string

--- indexing word string.py
+++ indexing word string.py
@@ -3,3 +3,4 @@ print(s.isalpha())
 print(s.isprintable())
 print(s.find("jungle"))
 print(s.index("to"))
+ #index is used to find the index of the word in the string
--- string lower upper capitalize.py
+++ string lower upper capitalize.py
@@ -6,3 +6,6 @@ print(a.upper())
 print(a.capitalize())
 print(b.capitalize())
 print(c.capitalize())
+#lower is used to convert the string into lowercase
+#upper is used to convert the string into uppercase
+#capitalize is used to convert the first letter of the string into uppercase and the rest into lowercase
